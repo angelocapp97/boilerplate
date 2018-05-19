8e94c519924210459f1390bf304eef0f835ddc3e
Angelo Cappelletti <angelocapp97@gmail.com>
2018-05-19 17:12:37 +0200

    Added babel-plugin-transform-object-rest-spread package

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import 'isomorphic-fetch'
+
+// Use example of babel-plugin-transform-object-rest-spread
+let house = {
+    bedrooms : 2,
+    bathrooms: 1.5,
+    yearBuilt: 2017
+}
+
+let newHouse = {
+    basement: true,
+    ...house
+}
+
+console.log(house)
+console.log(newHouse)
