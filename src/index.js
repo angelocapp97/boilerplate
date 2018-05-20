e15ddb8dad6d093d14178a914ba14a2bcfa3bb9e
Angelo Cappelletti <angelocapp97@gmail.com>
2018-05-20 10:49:55 +0200

    Added object & array destructuring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import 'isomorphic-fetch'
 
-// Use example of babel-plugin-transform-object-rest-spread
+// Use example of spread operator
 let house = {
     bedrooms : 2,
     bathrooms: 1.5,
@@ -14,3 +13,23 @@ let newHouse = {
 
 console.log(house)
 console.log(newHouse)
+
+// Use example of object & array destructuring
+const { basement, bedrooms, bathrooms, yearBuilt: year, details = 'No details provided' } = newHouse
+console.log(basement)
+console.log(bedrooms)
+console.log(bathrooms)
+console.log(year)
+console.log(details)
+
+const age = [65, 0, 13, 21, 0, 1, 2, 3]
+const [firstAge, , thirdAge, fourthAge, ...otherAges] = age
+console.log(firstAge)
+console.log(thirdAge)
+console.log(fourthAge)
+console.log(otherAges)
+
+const printNewHouse = ({ yearBuilt: year, bedrooms: rooms}) => {
+    console.log(`${year}: ${rooms}`)
+}
+printNewHouse(newHouse)
